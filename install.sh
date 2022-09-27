0c8b15dbab1811d9b5443c48f5213a56bdb72938
gsantella <gsantella@southhills.edu>
2022-09-27 09:56:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # upgrade system
+DEBIAN_FRONTEND=noninteractive
 apt update && apt upgrade -y
 
 # install nginx & jq & curl
@@ -10,23 +11,11 @@ mkdir /scripts
 
 # configure nginx
 rm /var/www/html/*
-wget -O /scripts # upgrade system
-apt update && apt upgrade -y
-
-# install nginx & jq & curl
-apt install nginx jq curl -y
-
-# create scripts directory
-rm -r /scripts
-mkdir /scripts
-
-# configure nginx
-rm -r /var/www/html/*
 
 # create script file & index page
 wget -O /scripts/update-iss-stats.sh https://raw.githubusercontent.com/gsantella/iss-server-scripts/main/update-iss-stats.sh
 
 # create crontab
-echo "* * * * * sh /scripts/script.sh" >> /var/tmp/cronconfig
+echo "* * * * * sh /scripts/update-iss-stats.sh" >> /var/tmp/cronconfig
 crontab /var/tmp/cronconfig
 rm /var/tmp/cronconfig
